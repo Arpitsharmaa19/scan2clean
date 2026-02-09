c7479eabc890f50440ca01791f378d32b7daab7a
arpit sharma <SS@MacBook-Air.local>
2026-02-09 22:46:48 +0530

    Debug: add diagnostic wrapper to citizen_dashboard to reveal 500 error cause

--- backend/build.sh
+++ backend/build.sh
@@ -2,9 +2,7 @@
 # exit on error
 set -o errexit
 cd "$(dirname "$0")"
-
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 python manage.py migrate
 python ensure_admin.py
